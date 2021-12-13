a2010e62284b0c4dc41dbc97baed345aee11f898
Rafael Bezerra da Escóssia Araújo <44624917+bezerraescossia@users.noreply.github.com>
2021-12-13 18:05:34 -0300

    Preparação ML

--- ml_preparation.py
+++ ml_preparation.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 # CARREGANDO E AJUSTANDO DATASET
 df = pd.read_csv('data/log.csv')
+df = df[['texto']]
 df[df['texto'].str.contains('COVID') | df['texto'].str.contains('CORONA') | df['texto'].str.contains('PANDEMIA')].reset_index(drop=True).to_csv('data/ml.csv', index=False)
 
 # INSERINDO LABELS
